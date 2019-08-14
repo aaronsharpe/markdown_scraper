2504dee6a3cc0897106ee0cc203e0ed4237d356a
aaronsharpe <aaronlsharpe@gmail.com>
2019-08-13 19:24:57 -0700

    added argparser

--- markdown_scraper.py
+++ markdown_scraper.py
@@ -1,54 +1,38 @@
 """
-Functions for scrapping markdown header cells from jupyter notebooks
+Functions for scraping markdown header cells from jupyter notebooks.
 """
 
 import json
-import os
+import argparse
+from pathlib import Path
 import nbformat as nbf
 
-def scrape_markdown(filelist=None, exclude=None,
-                    headers_only=False, skip_cells=0):
-    """
-    pulls all the markdown cells from jupyter notebooks
 
-    arguments:
-        filelist: list of notebooks to scrape
-        exclude: list of notebooks to skip
+def scrape_markdown(directory, headers_only=False, skip_cells=0):
+    """
+    Pulls all the markdown cells from jupyter notebooks.
+
+    Arguments:
+        directory: directory of notebooks to scrape
         headers_only: when true, skip plain markdown
         skip_cells: skips a number of markdown cells at the beginning of a file
     """
-    if filelist != None:
-        for file in filelist:
-            if not file.endswith('.ipynb'):
-                raise ValueError('filenames must end with .ipynb')
+    dir_path = Path(directory)
 
-    if filelist is None:
-        filelist = []
-        for file in os.listdir():
-            if file.endswith('.ipynb'):
-                filelist.append(file)
-        if exclude is not None:
-            for file in exclude:
-                if not file.endswith('.ipynb'):
-                    raise ValueError('filenames must end with .ipynb')
-                try:
-                    filelist.remove(file)
-                except ValueError:
-                    pass
+    filelist = filter(lambda path: path.suffix == '.ipynb', dir_path.glob('**/*'))
 
     markdown_cells = []
 
-    for file in filelist:
-        with open(file, 'r') as f:
-            json_load = json.load(f)
+    for file in list(filelist):
+        with open(file) as f:
+            notebook = json.load(f)
+        filename_cell = nbf.v4.new_markdown_cell('# ' + file.name)
+        markdown_cells.append(filename_cell)
 
-        cell = nbf.v4.new_markdown_cell('# ' + file)
-        markdown_cells.append(cell)
-
-        for i, cell in enumerate(json_load['cells']):
-            if cell['cell_type'] == 'markdown'and i > skip_cells-1:
-                if cell['source'] != []:
-                    if '#' in cell['source'][0]:
+        for i, cell in enumerate(notebook['cells']):
+            if cell['cell_type'] == 'markdown' and i > skip_cells-1:
+                if len(cell['source']) != 0:
+                    if cell['source'][0][0] == '#':
                         markdown = '#' + cell['source'][0]
                         cell = nbf.v4.new_markdown_cell(markdown)
                         markdown_cells.append(cell)
@@ -56,22 +40,37 @@ def scrape_markdown(filelist=None, exclude=None,
                         markdown = cell['source'][0]
                         cell = nbf.v4.new_markdown_cell(markdown)
                         markdown_cells.append(cell)
+
     return markdown_cells
 
 
 def gen_notebook(markdown_cells, filename='toc.ipynb'):
     """
-    generates a jupyter notebook comprised of the input markdown cells
+    Generates a jupyter notebook comprised of the input markdown cells.
 
-    arguments:
+    Arguments:
         markdown_cells: list of markdown cells to write
         filename: filename for the output notebook
     """
-    if not filename.endswith('.ipynb'):
-        raise ValueError('filenames must end with .ipynb')
 
     nb = nbf.v4.new_notebook()
     nb['cells'] = markdown_cells
 
     with open(filename, 'w') as f:
         nbf.write(nb, f)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('directory', help='directory to scrape')
+    parser.add_argument('--headers_only', type=bool, default=False,
+                        help='omit regular markdown cells')
+    parser.add_argument('-s', '--skip_cells', type=int, default=0,
+                        help='number of cells to skip at start of notebook')
+    parser.add_argument('-o', default='toc.ipynb', help='output filename')
+    args = parser.parse_args()
+
+    markdown_cells = scrape_markdown(args.directory,
+                                     headers_only=args.headers_only,
+                                     skip_cells=args.skip_cells)
+    gen_notebook(markdown_cells, filename=args.o)
